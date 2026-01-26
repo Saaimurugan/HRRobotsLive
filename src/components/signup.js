00ca059581b5592ac15d0208c10804884c33e386
Saai Murugan Purushothaman <saaimurugan@gmail.com>
2026-01-26 14:23:48 +0530

    fix(signup): update password validation regex to include hash character
    - Add hash character (#) to allowed special characters in password regex
    - Update character class in regex pattern to include # in both lookahead and character set
    - Improve password validation to support more special character options for users

--- src/components/signup.js
+++ src/components/signup.js
@@ -37,7 +37,7 @@ const SignUp = () => {
     };
 
     const validatePassword = (password) => {
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
         return passwordRegex.test(password);
     };
 
